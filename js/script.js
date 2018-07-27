fab6577c88a2a9eed605356c21ea1c397c08ca80
yuanjli <liyuanjienetwork@gmail.com>
2018-07-27 10:27:24 -0700

    problems with not getting the value or not have results

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
 console.log('Hello, front end');
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	console.log("DOM content has loaded!");
+	//script goes here
+
+	var x = document.getElementById("num").value;
+
+
+	console.log(x);
+});
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(){
+// 	let userInput = document.getElementById();
+// 	let resultSpan = document.getElementById();
+
+
+
+// })
+
+
+// //calulate the result 
+// function myFunction(){
+
+// 	console.log(document.getElementById("results").name);
+
+
+
+// 	document.getElementById("results").innerHTML = "hello world";
+	
+	// var x = document.getElementById("num").value;
+
+	// console.log(x);
+
+	// if (document.getElementById("ty").checked == true) {
+	// 	document.getElementById("result").innerHTML = x;  /*here should be F-> C */
+	// }else{
+	// 	document.getElementById("result").innerHTML = x;  /*here should be C -> F */
+	// }
+
